747a8fe38114d29bad9076c96bf5cad96282497c
souravbjp <souravbosu.bjp@zohomail.com>
2026-08-11 13:34:10 +0600

    Update environment variable defaults in config.py

--- config.py
+++ config.py
@@ -4,12 +4,12 @@ from typing import List
 API_ID = os.environ.get("API_ID", "")
 API_HASH = os.environ.get("API_HASH", "")
 BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-ADMIN = int(os.environ.get("ADMIN", "1255023013"))
+ADMIN = int(os.environ.get("ADMIN", "5408428203"))
 PICS = (os.environ.get("PICS", "https://i.ibb.co/MDssddJp/pic.jpg https://i.ibb.co/n8fQ2xcx/pic.jpg")).split()
-LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002686843200"))
+LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002392980176"))
 NEW_REQ_MODE = os.environ.get("NEW_REQ_MODE", "True").lower() == "true"
 DB_URI = os.environ.get("DB_URI", "")
-DB_NAME = os.environ.get("DB_NAME", "approve")
+DB_NAME = os.environ.get("DB_NAME", "autoacceptbot")
 IS_FSUB = os.environ.get("IS_FSUB", "False").lower() == "true"  # Set "True" For Enable Force Subscribe
 AUTH_CHANNELS = list(map(int, os.environ.get("AUTH_CHANNELS", "").split())) # Add Multiple channel ids
 AUTH_REQ_CHANNELS = list(map(int, os.environ.get("AUTH_REQ_CHANNELS", "").split())) # Add Multiple channel ids
